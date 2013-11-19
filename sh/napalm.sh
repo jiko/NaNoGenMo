2853a455b73ab1a31749715b4921b0c181211673
James Koppen <jk@jameskoppen.com>
2013-11-19 10:57:16 -0600

    Oulipo-inspired change: 7th word in line gets 7th dictionary entry below it.

--- sh/napalm.sh
+++ sh/napalm.sh
@@ -6,15 +6,21 @@
 
 count=0
 out=$2
+N=7
+dict="/usr/share/dict/words"
 touch $out
 while read line; do
   let count++
   if [ $(wc -w $out | tr -d [:alpha:][:punct:][:space:]) -lt 50000 ]; then
-    if [ $(( $count%9 )) -gt 3 ]; then
-      echo $line >> $out
-    else
-      echo "NAPALM" >> $out
-    fi
+      seventh=$(echo $line | cut -d " " -f $N)
+      linenum=$(grep -xn "$seventh" $dict | tr -d [:alpha:][:punct:])
+      if [ $linenum > 0 ]; then
+        let linenum-=7
+        replacement=$(awk -vlineNum=$linenum 'NR == lineNum { print $0 }' $dict)
+        echo ${line/$seventh/$replacement} >> $out
+      else
+        echo $line >> $out
+      fi
   else
     exit 0
   fi
